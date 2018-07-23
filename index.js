09e596d770083f200111fdbcf677a814d0fdfacf
weiting <a26007565@gmail.com>
2018-07-23 17:55:38 +0800

    Add command help and using async func

--- index.js
+++ index.js
@@ -1,16 +1,48 @@
 const fs = require('fs');
 const chineseConv = require('chinese-conv');
-fs.readdir('./', (err, data) => {
-    let files = [];
-    files = data.filter(function (file) {
-        return file.substr(-5) === '.json';
-    });
-    files.forEach((file) => {
-        fs.readFile(file, 'utf-8', (err, data) => {
-            fs.writeFile(file, chineseConv.sify(data), function (err) {
-                if (err) throw err;
-                console.log(`${file} Replaced!`);
-            });
+
+const args = process.argv.slice(2);
+let isTraditional = true;
+const help_page = `
+zh-converter
+https://github.com/a26007565/zh-converter
+
+Usage: zh-converter <command>
+Options:
+  -h, --help                    Display this information.
+  -t, --traditional             Convert to traditional chinese.
+  -s, --simplified              Convert to traditional chinese.
+`;
+
+for (let i = 0; i < args.length; i++) {
+  let arg = args[i];
+  if (arg === "--help" || arg === "-h") {
+    console.log(help_page);
+    process.exit();
+  } else if (arg === "--traditional" || arg === "-t") {
+    console.log(`converting to traditional chinese...\r\n`);
+    isTraditional = true;
+  } else if (arg === "--simplified" || arg === "-s") {
+    console.log(`converting to simplified chinese...\r\n`);
+    isTraditional = false;
+  } else {
+    console.log(help_page);
+    throw new Error(`invalid command line argument ${arg}`);
+  }
+}
+
+fs.readdir(process.cwd(), (err, data) => {
+  data.filter((file) => {
+    return file.substr(-5) === '.json';
+  }).map(async (file) => {
+    fs.readFile(file, 'utf-8', (err, data) => {
+      fs.writeFile(
+        file,
+        isTraditional ? chineseConv.tify(data) : chineseConv.sify(data),
+        (err) => {
+          if (err) throw err;
+          console.log(`${file} be converted!`);
         });
     });
+  });
 });
