79ea716ad85c35b8c05b83d3111808df547afe41
weiting <a26007565@gmail.com>
2018-08-02 15:07:05 +0800

    Improve get extension name

--- zh-converter.js
+++ zh-converter.js
@@ -1,10 +1,11 @@
 const fs = require('fs');
+const path = require('path');
 const chineseConv = require('chinese-conv');
 
 exports.run = (process, toTraditional = true) => {
   fs.readdir(process.cwd(), (err, data) => {
     data.filter((file) => {
-      return file.substr(-5) === '.json';
+      return path.extname(file) === '.json';
     }).map(async (file) => {
       await this.convertFile(file, toTraditional);
     });
